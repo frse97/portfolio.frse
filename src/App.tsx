fbfc878a33a92dfd2355b5984a61fb95455c6d96
FRSE <s.fries@wbi.at>
2021-01-08 23:57:56 +0100

    Initial commit for Porfolio Page. Ejected React App. Added Scss and Less support.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="frse-portfolio">
+      <div className="container">
+        Hi, my name is Sebastian Fries
+      </div>
+    </div>
+  );
+}
+
+export default App;
