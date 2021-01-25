12d0473a5636742252c1fcb50e75dc8dba95ce4a
Sebastian Fries <sebastian.fries@outlook.com>
2021-01-25 18:47:37 +0100

    Added power btn

--- src/components/CodeTyping/CodeTyping.tsx
+++ src/components/CodeTyping/CodeTyping.tsx
@@ -2,8 +2,15 @@ import React, { memo } from 'react';
 import './CodeTyping.scss';
 
 const CodeTyping: React.FC = () => {
-  return (<div className="typing-wrapper"><div className="frse-portfolio">
-  </div></div>);
+  return (
+  <div className="typing-wrapper">
+    <div className="frse-portfolio">
+    </div>
+    <div className="frse-portfolio-start">
+      <div className="frse-portfolio-power1"/>
+      <div className="frse-portfolio-power2"/>
+    </div>
+  </div>);
 }
 
 export default memo(CodeTyping);
