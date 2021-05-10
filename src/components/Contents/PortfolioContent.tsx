f33e0cf1cfc6d9d96b0bd9171e817e2a03201b17
Sebastian Fries <sebastian.fries@outlook.com>
2021-05-10 21:06:21 +0200

    Refactored styles. Fluid animation improvement (STABLE)

--- src/components/Contents/PortfolioContent.tsx
+++ src/components/Contents/PortfolioContent.tsx
@@ -16,10 +16,8 @@ const PortfolioContent: React.FC<IPortfolioContent> = props => {
   const portfolioContentClassName = cs("frse-portfolio-content", mode);
 
   return <div className={portfolioContentClassName}>
-    <FrseAvatar />
-    <div className="profile-description">
-      <p>Welcome to my mind</p>
-      <span>I am a webdeveloper with passion for every artistic aspect of life</span>
+    <div className="frse-portfolio-content-profile">
+      <FrseAvatar />
     </div>
   </div>
 }
