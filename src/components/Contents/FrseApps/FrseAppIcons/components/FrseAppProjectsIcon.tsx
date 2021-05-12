4f2f302d425b1bd12cd4ed1aadfc80d013ad6de2
Sebastian Fries <sebastian.fries@outlook.com>
2021-05-12 13:32:33 +0200

    Changed styles of Skills Icon, Added Projects Icon.

--- src/components/Contents/FrseApps/FrseAppIcons/components/FrseAppProjectsIcon.tsx
+++ src/components/Contents/FrseApps/FrseAppIcons/components/FrseAppProjectsIcon.tsx
@@ -5,7 +5,11 @@ interface IFrseAppProjectsIcon {}
 const FrseAppProjectsIcon: React.FC<IFrseAppProjectsIcon> = props => {
   return (
     <>
-      <div className="frse-projects-x" />
+      <div className="frse-app-icon-projects-arrow-left-1" />
+      <div className="frse-app-icon-projects-arrow-left-2" />
+      <div className="frse-app-icon-projects-arrow-middle" />
+      <div className="frse-app-icon-projects-arrow-right-1" />
+      <div className="frse-app-icon-projects-arrow-right-2" />
     </>
   )
 }
